f33a63f98cccfeb0e6aa29925dfff214a55285a7
Dhaval Thumar <dhaval.thumar@datavim.com>
2019-04-25 00:42:12 +0530

    view all question list with update each question using queid

--- stackoverflow-clone/app/views.py
+++ stackoverflow-clone/app/views.py
@@ -8,9 +8,11 @@ from django.template import RequestContext
 from datetime import datetime
 from django.urls import reverse
 from .forms import QuestionModelForm
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from . import models
 
 def home(request):
     """Renders the home page."""
@@ -74,20 +76,41 @@ def signup(request):
 
 def createQuestion(request):
     form = QuestionModelForm(request.POST or None)
-    print("++++++++++++++++++++++++++++++++++++++++++++++")
-    print("request:",request.method)
-    if(request.method == "POST"):
-        print("request.POST:",request.POST)
-    print("form:",form)
     if form.is_valid():
-        print("here")
         obj = form.save(commit=False)
         obj.createdby = request.user
         obj.save()
-        return HttpResponseRedirect(reverse('about'))
+        return HttpResponseRedirect(reverse('viewquestion'))
             
     context = {
         "form" : form,
     }
     template = "app/createquestion.html"
     return render(request, template, context)
+
+def updateQuestion(request, id=None):
+    obj = get_object_or_404(models.Question, queid = id)
+    form = QuestionModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        print("POST:",request.POST)
+        que = models.Question.objects.get(queid = id)
+        que.que_title = request.POST['que_title']
+        que.que_desc = request.POST['que_desc']
+        que.que_tag = request.POST['que_tag']
+        que.save()
+        return HttpResponseRedirect(reverse('viewquestion'))
+    
+    context = {
+        "form" : form,
+    }
+    template = "app/updatequestion.html"
+    return render(request, template, context)
+
+def viewQuestion(request):
+    obj = models.Question.objects.all()
+    context = {
+        "questions" : obj
+    }
+    template = "app/viewquestions.html"
+    return render(request, template, context)
+
