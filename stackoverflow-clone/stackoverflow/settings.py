8b624c914728ca25b83e176bd9841c58162c92ca
Dhaval Thumar <dhaval.thumar@datavim.com>
2019-04-26 13:19:04 +0530

    Sqlite3 DB support added

--- stackoverflow-clone/stackoverflow/settings.py
+++ stackoverflow-clone/stackoverflow/settings.py
@@ -78,6 +78,13 @@ WSGI_APPLICATION = 'stackoverflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# Alternative sqlite3 database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
